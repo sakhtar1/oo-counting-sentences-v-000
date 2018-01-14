e0ecd8df0361334c28c8b64671375c69615cc7c8
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 13:43:07 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,33 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-       true
-    else
-      false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-       true
-    else
-      false
-    end
-
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-       true
-    else
-      false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
-
-    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
-
+    self.scan(/[^\.!?]+[\.!?]/).count
   end
 end
